1a42b5cfde5b9e36a008086d26ee62bca8550dd5
Andor Hofecker <hof.and.or@gmail.com>
2020-01-06 18:37:43 +0100

    Fixed issues: - If a hashtag is clicked -> only show projects containing the hashtag, remove the others - Hover over hashtag - Empty lines in profile - Phone view - Min-height of boxes - Min-width of boxes - Profile widths adjusted - portfolio.py modified and portfolio.json generated - Profile text changed to white default

--- static/portfolio.js
+++ static/portfolio.js
@@ -1,21 +1,95 @@
 var selected = "";
 
-function hashtag_func(clicked) {
-  var hastags = document.getElementsByClassName("hashtag");
-  var clicked_element = clicked.textContent;
-  if (clicked_element == selected) {
-    for (let item of hastags) {
-      item.style.color = "white";
-    }
-    selected = "";
-  } else {
-    for (let item of hastags) {
-      if (item.textContent == clicked_element) {
-        item.style.color = "#ffff00";
-      } else {
-        item.style.color = "white";
-      }
-    }
-    selected = clicked_element;
+
+function remove_css_attribute(item, attribute) {
+  if (item.getAttribute("style").search(attribute)) {
+          item.style.removeProperty(attribute);
   }
 }
+
+
+function hashtag_clicked(active_hashtag) {
+  var hashtags = document.getElementsByClassName("hashtag");
+  var boxes = document.getElementsByClassName("box");
+  active_hashtag_text = active_hashtag.textContent.trim();
+
+  if (active_hashtag_text != selected) {
+
+    selected = active_hashtag_text;
+    // Find the boxes hashtags
+    [...boxes].forEach( function(box){
+      var box_hashtags = box.getElementsByClassName("hashtag");
+
+      // Create list of box hashtag texts
+      var box_hashtag_texts = [];
+
+      [...box_hashtags].forEach( function(box_hashtag){
+        box_hashtag_texts.push(box_hashtag.textContent.trim());
+      });
+
+      // Show only the relevant projects
+      if (!(box_hashtag_texts.includes(active_hashtag_text))) {
+        box.style.width = "0px";
+        box.style.display = "none";
+      } else {
+        box.style.removeProperty("width");
+        box.style.display = "flex";
+      }
+
+      // Highlight the selected hashtags
+      [...box_hashtags].forEach( function(box_hashtag){
+        box_hashtag_text = box_hashtag.textContent.trim();
+        if (box_hashtag_text == selected) {
+          box_hashtag.style.backgroundColor = "black";
+        } else {
+        // Remove highlight from not selected hashtags
+          remove_css_attribute(box_hashtag, "background-color");
+        }
+      });
+    });
+  } else {
+
+    // Find the boxes hashtags
+    [...boxes].forEach( function(box){
+      var box_hashtags = box.getElementsByClassName("hashtag");
+
+      // Show every project
+      remove_css_attribute(box, "width");
+      box.style.display = "flex";
+
+      // Remove text highlights
+      [...box_hashtags].forEach( function(box_hashtag) {
+        remove_css_attribute(box_hashtag, "background-color");
+      });
+
+      selected = "";
+
+    });
+  }
+};
+
+
+function hashtag_mouseover(active_hashtag) {
+
+  var hashtags = document.getElementsByClassName("hashtag");
+  var active_hashtag_text = active_hashtag.textContent;
+
+  for (let hashtag of hashtags) {
+    if (hashtag.textContent == active_hashtag_text) {
+      hashtag.style.color = "#ffff00";
+    }
+  }
+};
+
+
+function hashtag_mouseout(active_hashtag) {
+
+  var hashtags = document.getElementsByClassName("hashtag");
+  var active_hashtag_text = active_hashtag.textContent;
+
+  for (let hashtag of hashtags) {
+    if (hashtag.textContent == active_hashtag_text) {
+     hashtag.style.color = "white";
+    }
+  }
+};
